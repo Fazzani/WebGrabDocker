9a97573ac818da1b7b7dcb4f796fc3cf0bb8ca96
Fazzani <tunisienheni@outlook.com>
2018-04-22 18:57:37 +0200

    fix debug mode dockefile

--- WebGrabFromUrl/run.sh
+++ WebGrabFromUrl/run.sh
@@ -2,8 +2,12 @@
 set -e
 cd /app/wg++/bin || exit
 curl -fsSL ${WEBGRAB_CONFIG_URL} --output /config2/WebGrab++.config.xml || echo -e "Error while fetching webgrab config file"
-if [[ DEBUG=1 ]]; then 
+if [[ "$DEBUG" = "1" ]]; then 
   ls /config2 && cat /config2/WebGrab++.config.xml 
 fi
 mono WebGrab+Plus.exe  $1
+
+if [[ "$DEBUG" = "1" ]]; then 
+  ls /data &&  ls /data | head -1 | cat
+fi
 exit 0
