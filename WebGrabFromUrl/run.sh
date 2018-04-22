f84ac317ca8fc3a24c44d817785fc00e2d9e2428
Fazzani <tunisienheni@outlook.com>
2018-04-22 18:29:46 +0200

    fix dockerfile

--- WebGrabFromUrl/run.sh
+++ WebGrabFromUrl/run.sh
@@ -3,7 +3,8 @@
 cd /app/wg++/bin || exit
 curl ${WEBGRAB_CONFIG_URL} --silent  --output /config2/WebGrab++.config.xml
 if [[ DEBUG=1 ]]; then 
-  ls /config2 && cat /config/WebGrab++.config.xml 
+  set -eoux
+  ls /config2 && cat /config2/WebGrab++.config.xml 
 fi
 mono WebGrab+Plus.exe  $1
 exit 0
