41ce3daeee697f894260007a0ae1045e26bd9ad1
Heni FAZZANI <heni.fazzani@seloger.com>
2018-05-03 14:26:20 +0200

    add notif for webgrab finish

--- WebGrabFromUrl/run.sh
+++ WebGrabFromUrl/run.sh
@@ -6,6 +6,7 @@ if [[ "$DEBUG" = "1" ]]; then
   ls /config2 && cat /config2/WebGrab++.config.xml 
 fi
 mono WebGrab+Plus.exe  $1
+sudo wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=$1."  -qO-  > /dev/null 2>>4 &
 
 if [[ "$DEBUG" = "1" ]]; then 
   ls /data &&  ls /data | head -1 | cat
