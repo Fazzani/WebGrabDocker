04a104a4512fb32b4b843a229d396c9070c5f79c
Heni Fazzani <heni.fazzani@seloger.com>
2018-12-16 17:14:11 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone "https://Fazzani:${PAT}@github.com/Fazzani/webgrabplus-siteinipack.git" /siteinipack && \
-cp -rf /siteinipack/siteini.pack/ /config/
+cp -rf /siteinipack/siteini.pack/ $1
 
 cd /app/wg++/bin || exit
 
