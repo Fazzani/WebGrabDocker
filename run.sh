097cf89653febebba994bff2b497424fdd9b0298
Heni Fazzani <heni.fazzani@seloger.com>
2019-01-22 23:49:25 +0100

    fix debug mode

--- run.sh
+++ run.sh
@@ -9,10 +9,11 @@ cd /app/wg++/bin || exit
 
 mono WebGrab+Plus.exe $1
 
-if [[ "${DEBUG:-false}" == true ]]; then
-  echo "TZ: $TZ"
-  date
-  ls
+echo "TZ: $TZ"
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+echo $DATE
+
+if [[ ! -f "${1}/WebGrab++.log.txt" ]]; then
   cat "${1}/WebGrab++.log.txt"
 fi
 
